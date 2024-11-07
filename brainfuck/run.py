c297cb4be2a994d66cc39baa64132d51c866978d
Robert Rinia <robert@rinia.tv>
2024-11-07 20:43:31 +0100

    Significantly cleaned up run.py

--- brainfuck/run.py
+++ brainfuck/run.py
@@ -1,34 +1,13 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import subprocess
 
-og = open("one.h", "r")
-pl = og.read()
-og.close()
-lines = pl.splitlines(keepends=False)
-theline = lines[3]
-len_old = len(theline)
+p = subprocess.run(['clang-cpp-16', '-P', '-Wno-trigraphs', '-trigraphs', '-I', '.', '-I', '..', 'parse_program.h'], capture_output=True, text=True)
+pl = p.stdout
 
-i = 0
-j = 0
-print(theline)
 while True:
   p = subprocess.run(['clang-cpp-16', '-x',  'c', '-P', '-Wno-trigraphs', '-trigraphs', '-I', '.', '-I', '..', '-'], input=pl, capture_output=True, text=True)
   pl = p.stdout
-  lines = pl.splitlines(keepends=False)
-  theline2 = lines[3]
-  len_new = len(theline2)
-  if (len_new > len_old and len_new - len_old > 3):
-    theline = theline2
-    len_old = len(theline)
-    print(theline)
-  i = i + 1
-  if (i == 1000):
-    i = 0
-    out = open("logging/" + str(j) + ".log", "w")
-    out.write(pl)
-    out.close()
-    j = j + 1
   if ("Program terminated" in pl):
     final = """#include <character_table.h>
 #define PROCESS(a, b) a##b
@@ -41,13 +20,5 @@ TO_OUTPUT_
     pl = pl + final
     p = subprocess.run(['cpp', '-x',  'c', '-P', '-Wno-trigraphs', '-trigraphs', '-I', '.', '-I', '..', '-'], input=pl, capture_output=True, text=True)
     pl = p.stdout
-    p = subprocess.run(['cpp', '-x',  'c', '-P', '-Wno-trigraphs', '-trigraphs', '-I', '.', '-I', '..', '-'], input=pl, capture_output=True, text=True)
-    pl = p.stdout
-    out = open("logging/finished.log", "w")
-    out.write(pl)
-    out.close()
+    p = subprocess.run(['cpp', '-x',  'c', '-P', '-Wno-trigraphs', '-trigraphs', '-I', '.', '-I', '..', '-'], input=pl, stderr=subprocess.DEVNULL, text=True)
     break
-
-og = open("logging/finished.log", "r")
-print(og.read())
-og.close()
